28015d7f8edb8f5773ecb784cf44e18821ff683b
Jerome Anthony <jerome.pradeep@gmail.com>
2024-09-08 12:01:52 +1000

    sqllite integration

--- mydash.py
+++ mydash.py
@@ -2,12 +2,22 @@ from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 import ssl
+import sqlite3
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-df = pd.read_csv(
-    "http://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv"
-)
+# df = pd.read_csv(
+#     "http://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv"
+# )
+
+# Read all the customer records from the sqlite table customers
+con = sqlite3.connect("data/chinook.db")
+df = pd.read_sql_query("SELECT * from customers", con)
+
+# Verify that result of SQL query is stored in the dataframe
+print(df.head())
+
+con.close()
 
 app = Dash()
 
@@ -16,13 +26,13 @@ app.layout = [
     dcc.Input(id="input-box", type="text", value="What are you lookin for?", style={"width": "100%"}),
     # Search button
     html.Button("Search", id="search-button", style={"width": "100%"}),
-    dcc.Dropdown(df.country.unique(), "Canada", id="dropdown-selection"),
+    # dcc.Dropdown(df.country.unique(), "Canada", id="dropdown-selection"),
     dcc.Graph(id="graph-content"),
 ]
 
 @callback(Output("graph-content", "figure"), Input("dropdown-selection", "value"))
 def update_graph(value):
-    dff = df[df.country == value]
+    dff = df
     return px.line(dff, x="year", y="pop")
 
 
